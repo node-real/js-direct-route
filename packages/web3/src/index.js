6bf140c196f279123a7ba35ba3f1390d0620b616
cryyl <1226241521@qq.com>
2021-09-26 04:26:26 +0800

    fix dependency of web3
    Signed-off-by: cryyl <1226241521@qq.com>

--- packages/web3/src/index.js
+++ packages/web3/src/index.js
@@ -29,8 +29,8 @@
 
 
 var version = require('../package.json').version;
-var core = require('web3-core');
-var Eth = require('web3-eth');
+var core = require('@node-real/web3-core');
+var Eth = require('@node-real/web3-eth');
 var Net = require('web3-net');
 var Personal = require('web3-eth-personal');
 var Shh = require('web3-shh');
