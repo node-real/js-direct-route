40e35087c7e4ed74fbe52755448655fc962649b4
dean65 <dean950605@gmail.com>
2022-01-27 00:19:13 +0800

    update dependencies

--- packages/web3-core-requestmanager/src/batch.js
+++ packages/web3-core-requestmanager/src/batch.js
@@ -23,7 +23,7 @@
 "use strict";
 
 var Jsonrpc = require('./jsonrpc');
-var errors = require('web3-core-helpers').errors;
+var errors = require('@node-real/web3-core-helpers').errors;
 
 var Batch = function (requestManager) {
     this.requestManager = requestManager;
